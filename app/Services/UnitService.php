0db027b63b4bc2dc0917919196b7eb0a19149d62
yunenaychitun <ynct@thtelemedic.com>
2024-01-12 15:00:29 +0630

    added unit list

---
+++ app/Services/UnitService.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\CustomException;
+use App\Models\Unit;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Spatie\Permission\Models\Role;
+
+class UnitService
+{
+    public function model(): Unit
+    {
+        return new Unit();
+    }
+
+    public function paginate(?int $perPage = 15): LengthAwarePaginator
+    {
+        return $this->model()
+            ->latest()
+            ->paginate($perPage)
+            ->withQueryString();
+    }
+
+    // public function storeUser($request)
+    // {
+    //     try {
+    //         DB::beginTransaction();
+    //         $user = $this->createUser($request);
+    //         DB::commit();
+    //     } catch (\Exception $exception) {
+    //         DB::rollBack();
+    //     }
+    // }
+
+    // private function createUser($request): User
+    // {
+    //     $user = User::create([
+    //         'name' => $request->name,
+    //         'email' => $request->email ?? null,
+    //         'password' => Hash::make($request->password),
+    //     ]);
+    //     $role = Role::findById($request->role_id);
+    //     $roleName = $role->name;
+    //     $user->assignRole($roleName);
+    //     return $user;
+    // }
+
+    // public function updateUser(Request $request,User $user): User
+    // {
+    //     try {
+    //         DB::beginTransaction();
+    //         $user = $this->updateUserProfile($request, $user);
+    //         DB::commit();
+    //     } catch (\Exception $exception) {
+    //         DB::rollBack();
+
+    //         throw new CustomException($exception->getMessage(), $exception->getCode());
+    //     }
+
+    //     return $user;
+    // }
+
+    // private function updateUserProfile($request, User $user): User
+    // {
+    //     $user->update([
+    //         'name' => $request->name ?? $user->name,
+    //         'email' => $request->email ?? $user->email,
+    //         'password' => $request->password ? Hash::make($request->password) : $user->password,
+    //     ]);
+    //     $role = Role::findById($request->role_id);
+    //     $roleName = $role->name;
+    //     $user->syncRoles($roleName);
+    //     return $user;
+    // }
+}
